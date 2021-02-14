c19dc20ade4bb896bbbe9336323e5c57fe74e689
Laura Blechschmidt <laurablech02@gmail.com>
2021-02-14 14:03:10 +0100

    CHanged Button Color!

--- app/src/main/java/com/example/nutriscore/MainActivity.java
+++ app/src/main/java/com/example/nutriscore/MainActivity.java
@@ -5,21 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView1 = findViewById(R.id.textView1);
+        this.textView1 = findViewById(R.id.textView1);
         Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                textView1.setText("Moin was geht!");
-            }
-        });
+        button.setOnClickListener(this::buttonOnClick);
+        EditText sugarInput = findViewById(R.id.InputSugar);
     }
+
+    public void buttonOnClick(View v) {
+        this.textView1.setText("Ausgabe");
+    }
+
 
 }
