e4aa6cd7a59dc4961b2b0be0bc6a8a22d17e389c
Laura Blechschmidt <laurablech02@gmail.com>
2021-02-14 14:58:49 +0100

    Button fades Out!

--- app/src/main/java/com/example/nutriscore/MainActivity.java
+++ app/src/main/java/com/example/nutriscore/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonToShow;
     private TextView textView1;
     private EditText sugarInput;
     private Button calculate;
@@ -18,16 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.textView1 = findViewById(R.id.textView1);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(this::buttonOnClick);
+        this.buttonToShow = findViewById(R.id.button);
+        buttonToShow.setOnClickListener(this::buttonOnClick);
         this.sugarInput = findViewById(R.id.InputSugar);
         this.calculate = findViewById(R.id.button2);
         calculate.setOnClickListener(this::calculateOnClick);
     }
 
     public void buttonOnClick(View v) {
+        AlphaAnimation anim = new AlphaAnimation(1.0f, 0.0f);
+        anim.setDuration(1000);
+        buttonToShow.startAnimation(anim);
         this.sugarInput.setVisibility(View.VISIBLE);
         this.calculate.setVisibility(View.VISIBLE);
+        this.buttonToShow.setVisibility(View.INVISIBLE);
     }
 
     public void calculateOnClick(View v) {
