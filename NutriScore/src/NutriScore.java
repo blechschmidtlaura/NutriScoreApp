7dc7789a98eb10049a05783b9e0a759730411fe7
Johanna002 <79260489+Johanna002@users.noreply.github.com>
2021-02-18 18:06:48 +0100

    Add files via upload
    ergänzung: Ausgabe der Buchstaben

--- NutriScore/src/NutriScore.java
+++ NutriScore/src/NutriScore.java
@@ -8,7 +8,7 @@ public class NutriScore {
 
         System.out.println(nutriScore(149,5.7,0.5,0.76,6,5.3,7.9));
     }
-    public static int nutriScore(int energie, double zucker, double gesFettsaeuren, double natrium, int fruechteGemuese, double ballaststoffe, double eiweiss) {
+    public static String nutriScore(int energie, double zucker, double gesFettsaeuren, double natrium, int fruechteGemuese, double ballaststoffe, double eiweiss) {
         ScoreTabelle energieScore = new ScoreTabelle("Energiewert.txt");
         ScoreTabelle zuckerScore = new ScoreTabelle("Zuckerwert.txt");
         ScoreTabelle gesFettsaeurenScore = new ScoreTabelle("GesFettsaeuren.txt");
@@ -19,8 +19,22 @@ public class NutriScore {
 
         double punkte = energie + zucker + gesFettsaeuren + natrium;
         if (punkte >= 11 && (fruechteGemuese >= -4 && fruechteGemuese <= -1)) {
-            return energieScore.get(energie) + zuckerScore.get((float) zucker) + gesFettsaeurenScore.get((float) gesFettsaeuren) + natriumScore.get((float) natrium) + fruechteGemueseScore.get(fruechteGemuese) + ballaststoffeScore.get((float) ballaststoffe);
+            return nutriValue(energieScore.get(energie) + zuckerScore.get((float) zucker) + gesFettsaeurenScore.get((float) gesFettsaeuren) + natriumScore.get((float) natrium) + fruechteGemueseScore.get(fruechteGemuese) + ballaststoffeScore.get((float) ballaststoffe));
         }
-        return energieScore.get(energie) + zuckerScore.get((float) zucker) + gesFettsaeurenScore.get((float) gesFettsaeuren) + natriumScore.get((float) natrium) + fruechteGemueseScore.get(fruechteGemuese) + ballaststoffeScore.get((float) ballaststoffe) + eiweissScore.get((float) eiweiss);
+        return nutriValue(energieScore.get(energie) + zuckerScore.get((float) zucker) + gesFettsaeurenScore.get((float) gesFettsaeuren) + natriumScore.get((float) natrium) + fruechteGemueseScore.get(fruechteGemuese) + ballaststoffeScore.get((float) ballaststoffe) + eiweissScore.get((float) eiweiss));
+    }
+
+    public static String nutriValue(int value){
+        if(value <=-1) {
+            return "A";
+        }else if(value <=2){
+            return "B";
+        }else if(value <= 10){
+            return "C";
+        }else if(value <= 18){
+            return "D";
+        }else{
+            return "E";
+        }
     }
 }
