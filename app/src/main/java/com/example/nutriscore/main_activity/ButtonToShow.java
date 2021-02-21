dc4f176e88419448a4472f76f8bbf879b759ada6
Laura Blechschmidt <laurablech02@gmail.com>
2021-02-21 18:10:11 +0100

    refactored MainActivity

--- app/src/main/java/com/example/nutriscore/main_activity/ButtonToShow.java
+++ app/src/main/java/com/example/nutriscore/main_activity/ButtonToShow.java
@@ -1,26 +1,27 @@
 package com.example.nutriscore.main_activity;
 
+import android.os.Build;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 
+import androidx.annotation.RequiresApi;
+
+import java.util.List;
+
 public class ButtonToShow {
     private MainActivity mainActivity;
+    private List<View> viewsToMakeVisible;
 
-    public ButtonToShow(MainActivity mainActivity) {
+    public ButtonToShow(MainActivity mainActivity, List<View> viewsToMakeVisible) {
         this.mainActivity = mainActivity;
+        this.viewsToMakeVisible = viewsToMakeVisible;
         this.mainActivity.buttonToShow.setOnClickListener(this::onClick);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void onClick(View v) {
         this.fadeOutView(this.mainActivity.buttonToShow, this.mainActivity.BUTTON_TO_SHOW_FADE_OUT_DURATION);
-        this.mainActivity.sugarInput.setVisibility(View.VISIBLE);
-        this.mainActivity.energyInput.setVisibility(View.VISIBLE);
-        this.mainActivity.greaseInput.setVisibility(View.VISIBLE);
-        this.mainActivity.natriumInput.setVisibility(View.VISIBLE);
-        this.mainActivity.fruitVegetableInput.setVisibility(View.VISIBLE);
-        this.mainActivity.fibreInput.setVisibility(View.VISIBLE);
-        this.mainActivity.proteinInput.setVisibility(View.VISIBLE);
-        this.mainActivity.calculate.setVisibility(View.VISIBLE);
+        this.viewsToMakeVisible.forEach(view -> view.setVisibility(View.VISIBLE));
         this.mainActivity.buttonToShow.setVisibility(View.INVISIBLE);
     }
 
--- app/src/main/java/com/example/nutriscore/main_activity/MainActivity.java
+++ app/src/main/java/com/example/nutriscore/main_activity/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.nutriscore.main_activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.nutriscore.R;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public final int BUTTON_TO_SHOW_FADE_OUT_DURATION = 1000;
@@ -22,15 +28,20 @@ public class MainActivity extends AppCompatActivity {
     public EditText fibreInput;
     public EditText proteinInput;
     public Button calculate;
+
+    public List<View> viewsToMakeVisible = new LinkedList<>();
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.findViews();
-        ButtonToShow btn= new ButtonToShow(this);
+        this.viewsToMakeVisible = List.of(this.sugarInput, this.energyInput, this.fibreInput, this.fruitVegetableInput, this.greaseInput, this.natriumInput, this.proteinInput, this.calculate);
+        ButtonToShow btn= new ButtonToShow(this, viewsToMakeVisible);
         CalculateButton calculateButton = new CalculateButton(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.R)
     protected void findViews(){
         this.nutriScoreTextView = findViewById(R.id.textView1);
         this.sugarInput = findViewById(R.id.InputSugar);
