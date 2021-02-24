c13263bab8d8ba67a7965bc8110e2849b256375c
Tobias Bück <youremail@yourdomain.com>
2021-02-24 02:43:29 +0100

    BarCode Scanner works!

--- app/src/main/java/com/example/nutriscore/barcode_scanner/BarCodeScanner.java
+++ app/src/main/java/com/example/nutriscore/barcode_scanner/BarCodeScanner.java
@@ -3,6 +3,7 @@ package com.example.nutriscore.barcode_scanner;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
@@ -11,11 +12,28 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.example.nutriscore.R;
+import com.example.nutriscore.main_activity.MainActivity;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
@@ -30,15 +48,15 @@ public class BarCodeScanner extends AppCompatActivity {
     SurfaceView cameraView;
     CameraSource cameraSource;
     Button startButton;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_code_scanner);
-        startButton = findViewById(R.id.startCamera);
         cameraView = (SurfaceView) findViewById(R.id.camera_view);
-        barcodeInfo = (TextView) findViewById(R.id.txtContent);
-        startButton.setOnClickListener(this::onClick);
+        barcodeInfo = (TextView) findViewById(R.id.barCode);
+        startCamera();
     }
 
     protected void onClick(View v) {
@@ -51,71 +69,62 @@ public class BarCodeScanner extends AppCompatActivity {
 
         BarcodeDetector barcodeDetector =
                 new BarcodeDetector.Builder(this)
-                        .setBarcodeFormats(Barcode.CODE_128)//QR_CODE)
+                        .setBarcodeFormats(Barcode.ALL_FORMATS)//QR_CODE)
                         .build();
 
         cameraSource = new CameraSource
                 .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(640, 480)
+                .setAutoFocusEnabled(true)
                 .build();
 
         SurfaceHolder holder = cameraView.getHolder();
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            System.out.println("Permission");
-            return;
-        } else {
-            holder.addCallback(new SurfaceHolder.Callback() {
-                @SuppressLint("MissingPermission")
-                @Override
-                public void surfaceCreated(SurfaceHolder holder) {
-                    try {
-
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(BarCodeScanner.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                         cameraSource.start(cameraView.getHolder());
-                    } catch (IOException ie) {
-                        Log.e("CAMERA SOURCE", ie.getMessage());
+                    } else {
+                        ActivityCompat.requestPermissions(BarCodeScanner.this, new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
                     }
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", "LOOOOL" + ie.getMessage());
                 }
+            }
 
-                @Override
-                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
                 }
-
-                @Override
-                public void surfaceDestroyed(SurfaceHolder holder) {
-                    cameraSource.stop();
-                }
-            });
-
-
-            barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-                @Override
-                public void release() {
-                }
-
-                @Override
-                public void receiveDetections(Detector.Detections<Barcode> detections) {
-
-                    final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-
-                    if (barcodes.size() != 0) {
-                        barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
-                            public void run() {
-                                barcodeInfo.setText(    // Update the TextView
-                                        barcodes.valueAt(0).displayValue
-                                );
-                            }
-                        });
-                    }
-                }
-            });
-        }
+            }
+        });
     }
 }
