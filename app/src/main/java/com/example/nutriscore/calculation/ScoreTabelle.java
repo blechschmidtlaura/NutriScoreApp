1da8fd18da8f762263b1f4727e550a1b637fd44b
Tobias Bück <youremail@yourdomain.com>
2021-04-24 18:27:34 +0200

    added Javadoc für ScoreTabelle

--- app/src/main/java/com/example/nutriscore/calculation/ScoreTabelle.java
+++ app/src/main/java/com/example/nutriscore/calculation/ScoreTabelle.java
@@ -7,19 +7,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * ScoreTabelle
+ * Liest die Tabelle zur Berechnung des NutriScores aus einer Text Datei aus
+ */
 public class ScoreTabelle {
 
     private List<Integer> scores;
     private List<Float> bounds;
 
+    /**
+     * Initialisierung der ScoreTabelle
+     * @param inputStream Stream der Text Datei
+     */
     public ScoreTabelle(InputStream inputStream){
-        scores = new LinkedList<Integer>();
-        bounds = new LinkedList<Float>();
+        scores = new LinkedList<>();
+        bounds = new LinkedList<>();
         try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            BufferedReader input = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
             input.readLine(); //Kopfzeile ignorieren
             String zeile = null;
             while ((zeile = input.readLine()) != null){
@@ -52,6 +61,11 @@ public class ScoreTabelle {
         }
     }
 
+    /**
+     * Berechnet den Score für einen Wert input
+     * @param input der Wert
+     * @return der entsprechende Score für den input Wert
+     */
     public int get(double input) {
         for (int i = 0; i < scores.size(); i++) {
             if (input <= bounds.get(i)) {
